fa98b9c767c489e26eb8bb4cedc1884fa5300221
Constantin Nshuti <constantin.nshuti@mni.thm.de>
2022-11-22 23:27:04 +0100

    final again

--- HUE1/currency.ts
+++ HUE1/currency.ts
@@ -1,50 +1,82 @@
 let version : string = "04";
-let wert1;
-let wert2;
-let exchange;
+let text1 = (document.getElementById('wert1') as HTMLInputElement);
+let text2 = (document.getElementById('wert2') as HTMLInputElement);
+let backUp;
+let myExchangeResult = document.getElementById('neu-zeile') as HTMLElement;
+let i:number = 0;
+let parent_table = document.getElementById('table-laden') as HTMLElement;
+let show_table:boolean = false;
+let wert1: string;
+let wert2: string;
+let exchange: number;
 
-function start(event){
+function start(){
     document.getElementById("version").innerText = "Currency Calculator V-" + version;
 }
 
-function swap_currency(event){
-    document.getElementById('swapbutton').addEventListener('click', (event:any)=>{
-        event.preventDefault();
+function delete_input(parent: any){
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
 
-        wert1 = document.getElementById('wert1') as HTMLInputElement;
-        const text1 = wert1?.value;
-
-        wert2 = document.getElementById('wert2') as HTMLInputElement;
-
-        const text2 = wert2?.value;
-
-        exchange= (document.getElementById("resulat") as HTMLInputElement).value;
-
-        document.getElementById("neu-zeile").innerText = "Exchange rate: " + text2
-            + " / "+  text1 + " = " + exchange;
-
-    } )
 }
 
-function calculate_table(event){
-    event.preventDefault();
+function swap_currency(){
+    exchange =Number((document.getElementById("resulat") as HTMLInputElement).value);
+    // @ts-ignore
+    wert1 = String(text1.value);
+    // @ts-ignore
+    wert2 = String(text2.value);
 
-    wert1 = document.getElementById('wert1') as HTMLInputElement;
-    const text1 = wert1?.value;
+    text1.value = wert1;
+    text2.value = wert2;
+    if(i === 0){
+        myExchangeResult.innerHTML = `<p>Exchange rate ${wert1} /  ${wert2}  =  ${exchange.toFixed(2)}</p>`
+        if(show_table){
+            delete_input(parent_table);
+            show_table=false;
+        }
+        i=1
 
-    wert2 = document.getElementById('wert2') as HTMLInputElement;
-    const text2 = wert2?.value;
+    }else if(i===1){
+        console.log(i);
+        backUp = wert1;
+        wert1 = wert2;
+        wert2= backUp;
 
-        let table : string = '<table id="as-table"> '
+        text1.value = wert1;
+        text2.value = wert2;
+        myExchangeResult.innerHTML = `<p>Exchange rate ${wert1} /  ${wert2}  =  ${exchange.toFixed(2)}</p>`
+
+        if(show_table){
+            delete_input(parent_table);
+            show_table=false;
+        }
+
+        i=0;
+    }
+
+
+}
+
+function calculate_table(){
+
+    if(!show_table){
+
+        wert1 = (document.getElementById('wert1') as HTMLInputElement).value;
+        wert2 = (document.getElementById('wert2') as HTMLInputElement).value;
+
+        let element = document.createElement('div')
+
+        let table = '<table id="as-table"> '
             + '<tr class="first-line">'
-            + '<td >'+text1+'</td>'
-            + '<td>'+ text2+'</td>'
+            + '<td >'+wert1+'</td>'
+            + '<td>'+ wert2+'</td>'
             + '</tr>';
 
         let i=0;
 
         while(i < 1000){
-
             if(i < 10){
                 i++;
 
@@ -54,12 +86,14 @@ function calculate_table(event){
             }
             else if(i < 100){
                 i+=10;
+
                 wert2 = (exchange * i).toFixed(2);
                 table +='<tr class="first-100" >'+ '<td>'+i+'</td>'+'<td>'+wert2
                     +'</td>'+'</tr>' ;
             }
             else {
                 i+=100;
+
                 wert2 = (exchange * i).toFixed(2);
                 table +='<tr class="first-1000" >'+ '<td>'+i+'</td>'+'<td>'+wert2
                     +'</td>'+'</tr>' ;
@@ -67,24 +101,27 @@ function calculate_table(event){
             }
         }
         table += '</table>';
-        document.getElementById("table-laden").innerHTML = table;
+        element.innerHTML = table;
+        parent_table?.appendChild(element)
+
         document.getElementById('footer').innerText = 'Currency table';
 
 
+        show_table=true;
     }
 
-
+}
 
 document.addEventListener("DOMContentLoaded", (Event) => {
-    start(event);
-    swap_currency(event);
-
-
+    start();
+    document.getElementById('swapbutton').addEventListener('click', (event:any)=>{
+        event.preventDefault();
+        swap_currency()
+    })
 
     document.getElementById('calculate').addEventListener('click', (event:any)=> {
-
-        calculate_table(event);
-
+        event.preventDefault();
+        calculate_table();
     })
 
 })
