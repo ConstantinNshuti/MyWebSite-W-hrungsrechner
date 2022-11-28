b2b577a84c17c52297d14e8f29ecdf69dd03ef9d
Constantin Nshuti <constantin.nshuti@mni.thm.de>
2022-11-28 21:47:15 +0100

    Final Abgabe

--- HUE1/currency.ts
+++ HUE1/currency.ts
@@ -3,9 +3,9 @@ let text1 = (document.getElementById('wert1') as HTMLInputElement);
 let text2 = (document.getElementById('wert2') as HTMLInputElement);
 let backUp;
 let myExchangeResult = document.getElementById('neu-zeile') as HTMLElement;
-let i:number = 0;
+let i:number = 1;
 let parent_table = document.getElementById('table-laden') as HTMLElement;
-let show_table:boolean = false;
+let show_table:boolean = true;
 let wert1: string;
 let wert2: string;
 let exchange: number;
@@ -14,7 +14,7 @@ function start(){
     document.getElementById("version").innerText = "Currency Calculator V-" + version;
 }
 
-function delete_input(parent: any){
+function delete_table(parent: any){
     while (parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
@@ -30,15 +30,15 @@ function swap_currency(){
 
     text1.value = wert1;
     text2.value = wert2;
-    if(i === 0){
+    if(i === 1){
         myExchangeResult.innerHTML = `<p>Exchange rate ${wert1} /  ${wert2}  =  ${exchange.toFixed(2)}</p>`
         if(show_table){
-            delete_input(parent_table);
+            delete_table(parent_table);
             show_table=false;
         }
-        i=1
+        i=2
 
-    }else if(i===1){
+    }else if(i===2){
         console.log(i);
         backUp = wert1;
         wert1 = wert2;
@@ -48,12 +48,12 @@ function swap_currency(){
         text2.value = wert2;
         myExchangeResult.innerHTML = `<p>Exchange rate ${wert1} /  ${wert2}  =  ${exchange.toFixed(2)}</p>`
 
-        if(show_table){
-            delete_input(parent_table);
+       if(show_table){
+            delete_table(parent_table);
             show_table=false;
         }
 
-        i=0;
+        i=1;
     }
 
 
@@ -78,25 +78,37 @@ function calculate_table(){
 
         while(i < 1000){
             if(i < 10){
-                i++;
+                i=i+1;
+
+                let jedeZweite: String = 'first-10';
+                if (i === 2){                  // if ((i % 2) === 0){
+                    jedeZweite = 'farbe2zeile';
+                }
 
                 wert2 = (exchange * i).toFixed(2);
-                table +='<tr class="first-10" >'+ '<td>'+i+'</td>'+'<td>'+wert2
-                    +'</td>'+'</tr>' ;
+                table +=`<tr class= ${jedeZweite} > <td>${i}</td><td>${wert2}
+                    </td></tr>`;
+
             }
             else if(i < 100){
-                i+=10;
-
+                i=i+10;
+                let jedeZweite1: String = 'first-100';
+                if (i === 30){
+                    jedeZweite1 = 'farbe20zeile';
+                }
                 wert2 = (exchange * i).toFixed(2);
-                table +='<tr class="first-100" >'+ '<td>'+i+'</td>'+'<td>'+wert2
-                    +'</td>'+'</tr>' ;
+                table +=`<tr class= ${jedeZweite1} > <td>${i}</td><td>${wert2}
+                    </td></tr>`;
             }
             else {
                 i+=100;
-
+                let jedeZweite2: String = 'first-1000';
+                if (i === 300){
+                    jedeZweite2 = 'farbe200zeile';
+                }
                 wert2 = (exchange * i).toFixed(2);
-                table +='<tr class="first-1000" >'+ '<td>'+i+'</td>'+'<td>'+wert2
-                    +'</td>'+'</tr>' ;
+                table +=`<tr class= ${jedeZweite2} > <td>${i}</td><td>${wert2}
+                    </td></tr>`;
 
             }
         }
