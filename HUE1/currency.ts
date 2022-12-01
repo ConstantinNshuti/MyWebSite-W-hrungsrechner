d09dfb745aaccdf316418fc9da47e6fbd5b3e0f7
Constantin Nshuti <constantin.nshuti@mni.thm.de>
2022-12-01 10:56:24 +0100

    Final Abgabe1

--- HUE1/currency.ts
+++ HUE1/currency.ts
@@ -2,10 +2,11 @@ let version : string = "04";
 let text1 = (document.getElementById('wert1') as HTMLInputElement);
 let text2 = (document.getElementById('wert2') as HTMLInputElement);
 let backUp;
-let myExchangeResult = document.getElementById('neu-zeile') as HTMLElement;
-let i:number = 1;
+let myExchangeResult = document.getElementById('neu-zeile') as HTMLElement
+let myExchangeResult1 = document.getElementById('neu-zeile') as HTMLElement;
+//let i:number = 1;
 let parent_table = document.getElementById('table-laden') as HTMLElement;
-let show_table:boolean = true;
+//let show_table:boolean = false;
 let wert1: string;
 let wert2: string;
 let exchange: number;
@@ -14,64 +15,45 @@ function start(){
     document.getElementById("version").innerText = "Currency Calculator V-" + version;
 }
 
-function delete_table(parent: any){
-    while (parent.firstChild){
-        parent.removeChild(parent.firstChild);
-    }
-
-}
 
 function swap_currency(){
+
+
     exchange =Number((document.getElementById("resulat") as HTMLInputElement).value);
-    // @ts-ignore
-    wert1 = String(text1.value);
-    // @ts-ignore
-    wert2 = String(text2.value);
+    wert1 = (document.getElementById('wert1') as HTMLInputElement).value;
+    wert2 = (document.getElementById('wert2') as HTMLInputElement).value;
 
-    text1.value = wert1;
-    text2.value = wert2;
-    if(i === 1){
-        myExchangeResult.innerHTML = `<p>Exchange rate ${wert1} /  ${wert2}  =  ${exchange.toFixed(2)}</p>`
-        if(show_table){
-            delete_table(parent_table);
-            show_table=false;
-        }
-        i=2
-
-    }else if(i===2){
-        console.log(i);
         backUp = wert1;
         wert1 = wert2;
         wert2= backUp;
 
-        text1.value = wert1;
-        text2.value = wert2;
-        myExchangeResult.innerHTML = `<p>Exchange rate ${wert1} /  ${wert2}  =  ${exchange.toFixed(2)}</p>`
-
-       if(show_table){
-            delete_table(parent_table);
-            show_table=false;
-        }
-
-        i=1;
-    }
+       text1.value = wert1;
+       text2.value = wert2;
 
 
+        let x = 1/exchange;
+        //@ts-ignore
+        (document.getElementById("resulat") as HTMLInputElement).value = String(x);
+         myExchangeResult1.innerHTML = `<p>Exchange rate ${wert1} / ${wert2} = ${(1/exchange).toFixed(4)}</p>`
 }
 
 function calculate_table(){
 
-    if(!show_table){
+    parent_table.innerHTML = "";
 
         wert1 = (document.getElementById('wert1') as HTMLInputElement).value;
         wert2 = (document.getElementById('wert2') as HTMLInputElement).value;
 
+        exchange =Number((document.getElementById("resulat") as HTMLInputElement).value);
+        myExchangeResult.innerHTML = `<p>Exchange rate ${wert1} / ${wert2} = ${exchange.toFixed(4)}</p>`
+
+
         let element = document.createElement('div')
+
 
         let table = '<table id="as-table"> '
             + '<tr class="first-line">'
-            + '<td >'+wert1+'</td>'
-            + '<td>'+ wert2+'</td>'
+            + '<td >'+wert1+'</td>' + '<td>'+ wert2+'</td>'
             + '</tr>';
 
         let i=0;
@@ -81,33 +63,33 @@ function calculate_table(){
                 i=i+1;
 
                 let jedeZweite: String = 'first-10';
-                if (i === 2){                  // if ((i % 2) === 0){
+                if ((i % 2) === 0){
                     jedeZweite = 'farbe2zeile';
                 }
 
-                wert2 = (exchange * i).toFixed(2);
-                table +=`<tr class= ${jedeZweite} > <td>${i}</td><td>${wert2}
+                let y = (exchange * i).toFixed(2);
+                table +=`<tr class= ${jedeZweite} > <td>${i}</td><td>${y}
                     </td></tr>`;
 
             }
             else if(i < 100){
                 i=i+10;
                 let jedeZweite1: String = 'first-100';
-                if (i === 30){
+                if ((i % 20) !== 0){
                     jedeZweite1 = 'farbe20zeile';
                 }
-                wert2 = (exchange * i).toFixed(2);
-                table +=`<tr class= ${jedeZweite1} > <td>${i}</td><td>${wert2}
+                let y = (exchange * i).toFixed(2);
+                table +=`<tr class= ${jedeZweite1} > <td>${i}</td><td>${y}
                     </td></tr>`;
             }
             else {
                 i+=100;
                 let jedeZweite2: String = 'first-1000';
-                if (i === 300){
+                if ((i % 200) === 0){
                     jedeZweite2 = 'farbe200zeile';
                 }
-                wert2 = (exchange * i).toFixed(2);
-                table +=`<tr class= ${jedeZweite2} > <td>${i}</td><td>${wert2}
+                let y = (exchange * i).toFixed(2);
+                table +=`<tr class= ${jedeZweite2} > <td>${i}</td><td>${y}
                     </td></tr>`;
 
             }
@@ -116,19 +98,16 @@ function calculate_table(){
         element.innerHTML = table;
         parent_table?.appendChild(element)
 
-        document.getElementById('footer').innerText = 'Currency table';
+        document.getElementById('footer').innerText = ' Currency table';
 
-
-        show_table=true;
-    }
 
 }
 
 document.addEventListener("DOMContentLoaded", (Event) => {
     start();
-    document.getElementById('swapbutton').addEventListener('click', (event:any)=>{
-        event.preventDefault();
-        swap_currency()
+         document.getElementById('swapbutton').addEventListener('click', (event:any)=>{
+         event.preventDefault();
+         swap_currency()
     })
 
     document.getElementById('calculate').addEventListener('click', (event:any)=> {
